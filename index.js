ce62a9903414601cd5ca3ea3d14601636ad83589
urther <alswlkku@ajou.ac.kr>
2022-07-04 04:22:55 +0900

    feat : add login success feature
    - add login success feature

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+
+const corsOption = {
+  origin: "http://localhost:3000",
+  Credential: true,
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors(corsOption));
+
+app.get("/", (req, res) => {
+  console.log("hello");
+});
+
+app.post("/oAuth/kakao", async (req, res) => {
+  try {
+    const { access_token } = req.body;
+
+    const result = await axios({
+      url: "https://kapi.kakao.com/v2/user/me",
+      method: "get",
+      params: {
+        secure_resource: true,
+        property_key: ["kakao_account.email"],
+      },
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(8000, () => console.log("hello word!"));
